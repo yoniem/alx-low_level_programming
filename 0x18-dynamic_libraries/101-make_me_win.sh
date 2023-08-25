576dad31291568479d787b4d4bdad7527c629a33
yoniem <yonimphini@gmail.com>
2023-08-25 08:40:40 -0700

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-wget -P /tmp https://github.com/yoniemm/alx-low_level_programming/raw/master/0x018-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/yoniem/alx-low_level_programming/raw/master/0x018-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
